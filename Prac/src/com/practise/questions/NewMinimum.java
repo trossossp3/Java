9877318c249b61cc58a69349deb0e151316a04af
trossossp3 <21trossos@gmail.com>
2017-09-20 18:04:11 -0400

    i finished NewMinimum

--- Prac/src/com/practise/questions/NewMinimum.java
+++ Prac/src/com/practise/questions/NewMinimum.java
@@ -3,24 +3,24 @@ package com.practise.questions;
 import java.util.LinkedList;
 import java.util.Collections;
 import java.util.Scanner;
-import java.util.List;
+import java.util.ArrayList;
 
 public class NewMinimum {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		LinkedList<Integer> ints = new LinkedList<Integer>();
+		ArrayList<Integer> ints = new ArrayList<Integer>();
 		int n1 = sc.nextInt();
 		for (int i = 0; i < n1; i++) {
 			ints.add(sc.nextInt());
-			
-		} for (int i = 0; i < n1; i++) {
-			int n2 = (Collections.min(ints));
-			i
-			System.out.println(Collections.min(ints));
-	
-			
+
 		}
+		for (int i = 0; i < n1; i++) {
+			int n2 = (Collections.min(ints));
+			int n3 = ints.indexOf(Collections.min(ints));
+			System.out.println(n2);
+			ints.remove(n3);
 		
+		}
 
 	}
 
